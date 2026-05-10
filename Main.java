9185f0b1619fcb4541866aa11684a51c0909dae4
Shree A Ugale <shreeugale123@gmail.com>
2026-05-10 17:43:09 +0530

    abstract

---
+++ Main.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+abstract class Shape {
+	abstract double area();
+	abstract void display();
+}
+
+class Circle extends Shape {
+	double radius;
+	Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double area() {
+		return 3.14 * radius * radius;
+	}
+
+	public void display() {
+		System.out.println("Circle | Radius: " + radius + " | Area: " + area());
+	}
+}
+
+class Rectangle extends Shape {
+	double length, width;
+	Rectangle(double length, double width) { 
+		this.length = length;
+		this.width = width;
+	}
+
+	public double area() {
+		return length * width;
+	}
+
+	public void display() {
+		System.out.println("Rectangle | length: " + length + " Width: " + width + " | Area: " + area());
+	}
+}
+ 
+class Triangle extends Shape {
+	double base , height;
+	Triangle(double base, double height){
+		this.base = base;
+		this.height = height;
+	}
+	public double area(){
+		return 0.5 * base * height;
+	}
+	public void display() {
+		System.out.println("Triangle | Base: " + base + " Height: " +height+ " | Area: " + area());
+	}
+
+}
+class Main {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		Shape[] shapes = new Shape [3];
+		shapes[0]= new Circle(5);
+		 shapes[1] = new Rectangle(4, 3);
+        shapes[2] = new Triangle(6, 4);
+
+        System.out.println("Choose shape: 1.Circle  2.Rectangle  3.Triangle");
+	System.out.print("Enter your choice: ");
+        int choice = reader.nextInt();
+
+        
+        System.out.println("\n===== Shape Details =====");
+        shapes[choice - 1].display();
+        System.out.println("=========================");
+
+        reader.close(); 
+    }
+}
+
